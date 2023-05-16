7b1445a3d52acf63dff0e2b476efe2f19db644d8
Varokas Panusuwan <varokas@panusuwan.com>
2023-05-15 21:45:55 -0700

    add params

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import boto3
 from botocore.config import Config
 import json
 
+from params_aws import params_aws
+from params_aws import model
+
 REGION="ap-southeast-1"
 
 app = FastAPI(title="python-serverless")
@@ -12,13 +15,16 @@ app = FastAPI(title="python-serverless")
 def hello():
     return {"hello": "world"}
 
-@app.get("/params")
-def params():
+@app.get("/params_raw")
+def params_raw():
     client = boto3.client('ssm', config=Config(region_name=REGION))
     response = client.get_parameters(Names=["test"])
     params = response["Parameters"]
 
     return {p["Name"]:json.loads(p["Value"]) for p in params}
 
+@app.get("/params")
+def params():
+    return params_aws.get_parameter(name='test', modelType=model.DBConfig)
 
 handler = Mangum(app)
